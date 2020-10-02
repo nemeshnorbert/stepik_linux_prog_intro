fb49f4e6f91a0807e22da3a64831537f7210f565
Norbert Nemesh <nemeshnorbert@gmail.com>
2020-10-02 07:28:36 +0000

    Add solution for the task 1_2_8

---
+++ task_1_2_8/solution.c
@@ -0,0 +1,8 @@
+#include <stddef.h>
+#include <string.h>
+#include "solution.h"
+
+int stringStat(const char *string, size_t multiplier, int *count) {
+    *count += 1;
+    return strlen(string) * multiplier;
+}
---
+++ task_1_2_8/solution.h
@@ -0,0 +1,6 @@
+#ifndef __SOLUTION_H__
+#define __SOLUTION_H__
+
+int stringStat(const char *string, size_t multiplier, int *count);
+
+#endif  // __SOLUTION_H__
